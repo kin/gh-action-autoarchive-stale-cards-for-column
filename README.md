b87866657c2a071c26c52afedaffd6c91cdf4a5c
Jeremy Ward <celloward@gmail.com>
2020-11-16 12:22:23 -0600

    Add action

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # gh-action-autoarchive-issues-for-column
+This action allows the archiving of stale project cards in a given Github Project column. Helpful especially for cleaning up columns like "Done"
+
+## Use
+- Create a .yml workflow in `.github/workflows` directory of the repo that will run the action.
+- Follow the below template to use the action and make sure to fill out all required environment variables.
+
+## Workflow Template
+```
+name: Archive Old Done Cards
+
+on:
+  # Any trigger is usable here. This will run the action every day at 5AM.
+  schedule:
+    - cron: '0 5 * * *'
+
+jobs:
+  autoarchive-cards-job:
+    runs-on: ubuntu-latest
+    name: Autoarchive Done Column Cards
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+      - name: The autoarchive action
+        uses: kin/gh-action-autoarchive-issues-for-column@v1.0
+        id: archive
+        with:
+          # Required: personal access token with permissions to archive cards
+          access-token: "${{ secrets.GITHUB_TOKEN }}"
+
+          # Required: String with repo name (e.g. 'kin/gh-action-autoarchive-issues-for-column' should be 'gh-action-autoarchive-issues-for-column' here)
+          repository: 'my-repo-name'
+
+          # Required: String with owner name only (e.g. 'kin/gh-action-autoarchive-issues-for-column' should be 'kin' here)
+          repository-owner: 'the-owner'
+
+          # Required: String with name of project the column is in (case insensitive)
+          project-name: 'My Project'
+
+          # Required: String with column name for (case insensitive)
+          column-to-archive: 'Done'
+
+          # Required: Integer for how many days must have elapsed since the last time this card was touched
+          days-old: 5
+```
+
+## Contribution
+To cotnribute, please open an Issue on the action repo: https://github.com/kin/gh-action-autoarchive-issues-for-column to discuss bugs/modifications.
---
+++ index.js
@@ -0,0 +1,128 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { graphql } = require("@octokit/graphql");
+const isBefore = require('date-fns/isBefore')
+const sub = require('date-fns/sub')
+
+const fetchCardsQuery = `query projectCards($owner: String!, $repo: String!, $projectName: String!, $cursor: String!) {
+                           repository(owner: $owner, name: $repo) {
+                             projects(search: $projectName, last: 1) {
+                               edges {
+                                 node {
+                                   columns(first: 20) {
+                                     edges {
+                                       node {
+                                         name
+                                         cards(first: 50, after: $cursor, archivedStates: NOT_ARCHIVED) {
+                                           edges {
+                                             node {
+                                               id
+                                               updatedAt
+                                             }
+                                             cursor
+                                           }
+                                           pageInfo {
+                                             endCursor
+                                             hasNextPage
+                                           }
+                                         }
+                                       }
+                                     }
+                                   }
+                                 }
+                               }
+                             }
+                           }
+                         }`
+
+const archiveCardQuery = `mutation archiveCards($cardId: String!, $isArchived: Boolean = true) {
+                             updateProjectCard(input:{projectCardId: $cardId, isArchived: $isArchived}) {
+                               projectCard {
+                                 id
+                               }
+                             }
+                           }`
+
+async function fetchCards(repoOwner, repo, projectName, currentCursor, accessToken) {
+  return graphql(fetchCardsQuery, {
+    owner: repoOwner,
+    repo: repo,
+    projectName: projectName,
+    cursor: currentCursor,
+    headers: {
+      authorization: `bearer ${accessToken}`,
+    }
+  })
+}
+
+
+const dateifyCard = (card) => {
+  const updatedAt = new Date(card.updatedAt)
+  return { id: card.id, updatedAt: updatedAt }
+}
+
+async function fetchCardInfo(repoOwner, repo, projectName, accessToken, columnToArchive) {
+  try {
+    const projectCardIdsWithDate = []
+    let currentCursor = ''
+    let nextPage = true
+
+    while(nextPage) {
+      let projectCards = await fetchCards(repoOwner, repo, projectName, currentCursor, accessToken)
+      projectCards = projectCards.repository.projects.edges[0].node.columns.edges.find(edge => edge.node.name.toLowerCase() === columnToArchive.toLowerCase()).node.cards
+      projectCardIdsWithDate.push(...projectCards.edges.flatMap(card => card.node).map(dateifyCard))
+
+      currentCursor = projectCards.pageInfo.endCursor
+      nextPage = projectCards.pageInfo.hasNextPage
+    }
+
+    return projectCardIdsWithDate
+  }
+  catch(e) {
+    console.log('fetchCardInfo error: ', e)
+    return []
+  }
+}
+
+
+const run = async () => {
+  try {
+    const accessToken = core.getInput('access-token')
+    const columnToArchive = core.getInput('column-to-archive')
+    const repoOwner = core.getInput('repository-owner')
+    const repo = core.getInput('repository')
+    const projectName = core.getInput('project-name')
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+    const daysOld = core.getInput('days-old');
+    const cutoffDate = sub(new Date(), { days: daysOld })
+
+    console.log(`Archiving all cards that have been untouched for ${daysOld} days from column ${columnToArchive}!`);
+
+    console.log(`The event payload: ${payload}`);
+
+    const projectCardIdsWithDate = await fetchCardInfo(repoOwner, repo, projectName, accessToken, columnToArchive)
+
+    // Filter by updated at date
+    const cardIdsToArchive = projectCardIdsWithDate
+          .filter(card => isBefore(card.updatedAt, cutoffDate))
+          .map(node => node.id)
+
+    // Archive those - https://docs.github.com/en/free-pro-team@latest/rest/reference/projects#update-an-existing-project-card
+    cardIdsToArchive.forEach(async (id) => {
+      await graphql(archiveCardQuery, {
+        cardId: id,
+        headers: {
+          authorization: `bearer ${accessToken}`,
+        },
+      })
+    })
+
+    console.log(`Archived ${cardIdsToArchive.length} cards`)
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
+}
+
+run()
